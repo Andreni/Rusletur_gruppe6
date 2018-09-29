a3bc64b8e59a0484754b9e33c586876296a1f660
Andreas Nielsen <andreni@hiof.no>
2018-09-29 11:43:43 +0200

    Setup methods

--- app/src/main/java/no/hiof/informatikk/gruppe6/rusletur/MainActivity.java
+++ app/src/main/java/no/hiof/informatikk/gruppe6/rusletur/MainActivity.java
@@ -2,8 +2,14 @@ package no.hiof.informatikk.gruppe6.rusletur;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+
+import com.google.firebase.auth.FirebaseAuth;
 
 public class MainActivity extends AppCompatActivity {
+
+    private FirebaseAuth mAuth;
+    final String logTagUser = "Login";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -14,5 +20,19 @@ public class MainActivity extends AppCompatActivity {
         //TODO 1.1 Make simple login screen:
         //TODO 1.2 Make registration page:
         //TODO 1.3 If statement to check if user is logged in
+    }
+
+    //Method for signing up user
+    public void registerUser(View view){
+
+
+    }
+
+    public void checkUserInput(Boolean validInput){
+
+    }
+
+    public void writeToastToUser(String messageToUser){
+
     }
 }
