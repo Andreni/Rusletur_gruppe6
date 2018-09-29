2fd1341fa91ee3eaf090b3f078726acb7cbe760c
Andreas Nielsen <andreni@hiof.no>
2018-09-29 14:25:23 +0200

    Added login_gui

--- app/src/main/java/no/hiof/informatikk/gruppe6/rusletur/MainActivity.java
+++ app/src/main/java/no/hiof/informatikk/gruppe6/rusletur/MainActivity.java
@@ -2,6 +2,7 @@ package no.hiof.informatikk.gruppe6.rusletur;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -14,5 +15,10 @@ public class MainActivity extends AppCompatActivity {
         //TODO 1.1 Make simple login screen:
         //TODO 1.2 Make registration page:
         //TODO 1.3 If statement to check if user is logged in
+        //TODO 1.4 Make horizontal layout
+    }
+
+    public void loginUser(View view){
+
     }
 }
