67928c47c439f44b9e5e3dae29cbd4f51abcf218
Andreas Nielsen <andreni@hiof.no>
2018-09-29 15:54:52 +0200

    Added commits

--- app/src/main/java/no/hiof/informatikk/gruppe6/rusletur/MainActivity.java
+++ app/src/main/java/no/hiof/informatikk/gruppe6/rusletur/MainActivity.java
@@ -1,11 +1,16 @@
 package no.hiof.informatikk.gruppe6.rusletur;
 
+import android.support.annotation.NonNull;
 import android.support.constraint.ConstraintLayout;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.EditText;
+import android.widget.Toast;
 
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
 
@@ -14,15 +19,24 @@ public class MainActivity extends AppCompatActivity {
     EditText edPass;
     EditText edEmail;
     ConstraintLayout loginPage;
+    ConstraintLayout registerPage;
+    EditText inputEmail;
+    EditText inputPassword;
+    EditText secondInputPassword;
+
 
     private FirebaseAuth mAuth;
-    private FirebaseUser mAUser;
+    private FirebaseUser mUser;
 
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        mAuth = FirebaseAuth.getInstance();
+
+        //Initialize elements:
 
         //TODO 1.1 Make simple login screen:
         //TODO 1.2 Make registration page:
@@ -34,29 +48,88 @@ public class MainActivity extends AppCompatActivity {
 
     //Click from button
     public void loginUser(View view){
+        //TODO Animate login process
 
-        if(checkForValidUserInput()){
+        if(checkForValidUserInput(1)){
             mAuth.signInWithEmailAndPassword(edEmail.getText().toString()
-                    ,edPass.getText().toString());
+                    ,edPass.getText().toString())
+                    .addOnCompleteListener(new OnCompleteListener<AuthResult>() {
+                        @Override
+                        public void onComplete(@NonNull Task<AuthResult> task) {
+                            if(task.isSuccessful()){
+                                //TODO Make an intent that sends the user to the second activity
+                            }else{
+                                //If the login process is interrupted: warn user:
+                                writeMessageToUser(task.getException().toString());
+                            }
+                        }
+                    });
         }
     }
 
     //Click from button
     public void registerUser(View view){
+        //Hide login page
+        loginPage.setVisibility(View.INVISIBLE);
+        registerPage.setVisibility(View.VISIBLE);
+        if(checkForValidUserInput(2)){
+            mAuth.createUserWithEmailAndPassword(inputEmail.getText().toString(),
+                    inputPassword.getText().toString())
+                    .addOnCompleteListener(new OnCompleteListener<AuthResult>() {
+                        @Override
+                        public void onComplete(@NonNull Task<AuthResult> task) {
+                            if(task.isSuccessful()){
+                                writeMessageToUser("Du er registrert :)");
 
+                            }else{
+                                writeMessageToUser(task.getException().toString());
+                            }
+                        }
+                    });
+        }
 
     }
 
     //Check if field values entered are correct:
-    private boolean checkForValidUserInput(){
+    //Option 1 checks loginpage for valid input
+    //Option 2 checks registerpage for valid input
+    private boolean checkForValidUserInput(int option){
 
-        if ((edEmail.toString()!=null)&&(edPass!=null)){
-
-            if(edPass.toString().length()>5){
-
-                return true;
+        if(option==1) {
+            if ((edEmail.toString() != null || edEmail.toString().length() > 0)
+                    && (edPass != null)) {
+                if (edPass.toString().length() > 5) {
+                    return true;
+                } else {
+                    //TODO Write static variable
+                    writeMessageToUser("Passordet er for kort.");
+                    return false;
+                }
+            } else {
+                //TODO Write static variable
+                writeMessageToUser("Kontroller hva du har skrevet inn");
+                return false;
             }
+        }else if(option == 2){
+            if ((inputEmail.toString() != null && inputEmail.toString().length() > 0)){
+                if((inputPassword.toString()!=null)
+                        &&inputPassword.toString()==secondInputPassword.toString()){
+                    return true;
+                }
+            }else{
+                writeMessageToUser("Ugyldig input");
+                return false;
+            }
+
+
         }
+
+        //Input is faulty!
         return false;
+    }
+
+    //Send message to user:
+    private void writeMessageToUser(String messageToUser){
+        Toast.makeText(this,messageToUser.toString(),Toast.LENGTH_SHORT).show();
     }
 }
