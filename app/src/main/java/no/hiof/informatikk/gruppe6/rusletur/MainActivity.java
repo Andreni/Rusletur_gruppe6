b4d4dbea1668d2871aef93c4d487764c479d81ea
Andreas Nielsen <andreni@hiof.no>
2018-09-29 14:55:19 +0200

    Method implementation

--- app/src/main/java/no/hiof/informatikk/gruppe6/rusletur/MainActivity.java
+++ app/src/main/java/no/hiof/informatikk/gruppe6/rusletur/MainActivity.java
@@ -1,10 +1,23 @@
 package no.hiof.informatikk.gruppe6.rusletur;
 
+import android.support.constraint.ConstraintLayout;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.EditText;
+
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
 
 public class MainActivity extends AppCompatActivity {
+
+    EditText edPass;
+    EditText edEmail;
+    ConstraintLayout loginPage;
+
+    private FirebaseAuth mAuth;
+    private FirebaseUser mAUser;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -15,10 +28,36 @@ public class MainActivity extends AppCompatActivity {
         //TODO 1.1 Make simple login screen:
         //TODO 1.2 Make registration page:
         //TODO 1.3 If statement to check if user is logged in
-        //TODO 1.4 Make horizontal layout
+        //TODO 1.4 Make horizontal layout (Non priority)
+
+
     }
 
+    //Click from button
     public void loginUser(View view){
 
+        if(checkForValidUserInput()){
+            mAuth.signInWithEmailAndPassword(edEmail.getText().toString()
+                    ,edPass.getText().toString());
+        }
+    }
+
+    //Click from button
+    public void registerUser(View view){
+
+
+    }
+
+    //Check if field values entered are correct:
+    private boolean checkForValidUserInput(){
+
+        if ((edEmail.toString()!=null)&&(edPass!=null)){
+
+            if(edPass.toString().length()>5){
+
+                return true;
+            }
+        }
+        return false;
     }
 }
