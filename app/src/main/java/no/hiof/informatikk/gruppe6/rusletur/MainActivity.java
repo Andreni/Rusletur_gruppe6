40929b6f0aaffa73c317991b638620a7aa574521
Andreas <amikal@hotmail.no>
2018-09-30 14:57:03 +0200

    added actionbar to main activity

--- app/src/main/java/no/hiof/informatikk/gruppe6/rusletur/MainActivity.java
+++ app/src/main/java/no/hiof/informatikk/gruppe6/rusletur/MainActivity.java
@@ -5,6 +5,9 @@ import android.support.annotation.NonNull;
 import android.support.constraint.ConstraintLayout;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
 import android.view.View;
 import android.widget.EditText;
 import android.widget.Toast;
@@ -59,6 +62,37 @@ public class MainActivity extends AppCompatActivity {
 
             //TODO 1.4 Make horizontal layout (Non priority)
 
+        }
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        MenuInflater inflater = getMenuInflater();
+        inflater.inflate(R.menu.actionbar_menu, menu);
+        getSupportActionBar().setTitle("RusleTur");
+        return true;
+    }
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+
+        // TODO 1.0: Add button action for actionbar
+
+        switch(item.getItemId()) {
+            //TODO 1.1: Add action for home button
+            case R.id.action_home:
+                //Intent intent = new Intent(this, home.class);
+                //startActivity(intent);
+                //return true;
+                writeMessageToUser("Home clicked");
+
+                //TODO 1.2: Add action for settings button
+            case R.id.action_settings:
+                //Intent intent = new Intent(this, settings.class);
+                //startActivity(intent);
+                //return true;
+                writeMessageToUser("Settings clicked");
+            default:
+                return super.onOptionsItemSelected(item);
         }
     }
 
