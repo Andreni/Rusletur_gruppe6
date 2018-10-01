7b0fe0b13fa3aaaf318fd18a084cbca85a0c0100
Andreas <amikal@hotmail.no>
2018-10-01 19:37:28 +0200

    Constructor fix, added setter and getter

--- app/src/main/java/no/hiof/informatikk/gruppe6/rusletur/Trip.java
+++ app/src/main/java/no/hiof/informatikk/gruppe6/rusletur/Trip.java
@@ -1,17 +1,40 @@
 package no.hiof.informatikk.gruppe6.rusletur;
 
+import com.google.android.gms.maps.model.LatLng;
+
+import java.util.ArrayList;
+
 public class Trip {
 
     private String name, description, author;
-    private 
+    private ArrayList<LatLng> coordinates;
 
-    public Trip(String name, String author) {
+    public Trip(String name, String author, ArrayList<LatLng> coordinates) {
         this.name = name;
         this.author = author;
-        addCoordinates();
+        this.coordinates = coordinates;
     }
 
-    private void addCoordinates(){
+    public void setName(String name) {
+        this.name = name;
+    }
+    public void setDescription(String description) {
+        this.description = description;
+    }
+    public void setAuthor(String author) {
+        this.author = author;
+    }
 
+    public String getName() {
+        return name;
+    }
+    public String getDescription() {
+        return description;
+    }
+    public String getAuthor() {
+        return author;
+    }
+    public ArrayList<LatLng> getCoordinates() {
+        return coordinates;
     }
 }
