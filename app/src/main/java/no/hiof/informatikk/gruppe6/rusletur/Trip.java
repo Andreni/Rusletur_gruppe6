570ac5407c3f1f2a77f29b69d26007edc5a2f663
Andreas <amikal@hotmail.no>
2018-10-01 19:29:41 +0200

    Adde d constructure

--- app/src/main/java/no/hiof/informatikk/gruppe6/rusletur/Trip.java
+++ app/src/main/java/no/hiof/informatikk/gruppe6/rusletur/Trip.java
@@ -1,4 +1,17 @@
 package no.hiof.informatikk.gruppe6.rusletur;
 
 public class Trip {
+
+    private String name, description, author;
+    private 
+
+    public Trip(String name, String author) {
+        this.name = name;
+        this.author = author;
+        addCoordinates();
+    }
+
+    private void addCoordinates(){
+
+    }
 }
