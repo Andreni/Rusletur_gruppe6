d85e59b5a0c0169c4be2a6f7d218682bd5dd7a6e
Magnus <magnus.d.pettersen@hotmail.com>
2018-10-03 20:51:11 +0200

    Created Debug with ability to add stuff to database

--- app/src/main/java/no/hiof/informatikk/gruppe6/rusletur/UserManagmentDebug.java
+++ app/src/main/java/no/hiof/informatikk/gruppe6/rusletur/UserManagmentDebug.java
@@ -3,16 +3,25 @@ package no.hiof.informatikk.gruppe6.rusletur;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
+import android.view.View;
+import android.widget.EditText;
 import android.widget.TextView;
 
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
 
 public class UserManagmentDebug extends AppCompatActivity {
 
-    private static final String TAG = "UserManagmentDebug.class";
+    private static final String TAG = "UserManagmentDebug";
     FirebaseUser mUser;
     FirebaseAuth mAuth;
+
+    FirebaseDatabase database = FirebaseDatabase.getInstance();
+    DatabaseReference myRef = database.getReference("id");
+
+    int counter = 0;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -29,7 +38,24 @@ public class UserManagmentDebug extends AppCompatActivity {
             Log.e(null, mUser.getEmail());
         }
 
+    }
 
+    public void sendInfoToDatabase(View view) {
+        Log.d(TAG, "sendInfoToDatabase exectued");
+        EditText textToSend = findViewById(R.id.debugInputToSendInToDatabase);
+        if(counter == 1) {
+            myRef.setValue(String.valueOf(textToSend.getText()));
+            counter = 0;
+        }
+        else {
+            try {
+                Log.i(TAG, String.valueOf(textToSend.getText()));
+            }
+            catch(NullPointerException e) {
+                Log.e(TAG, "Text from input was NULL");
+            }
+            counter++;
+        }
 
     }
 
