e46eb6b9e48e1f75cb0b53ab00d2ac417d763534
Magnus <magnus.d.pettersen@hotmail.com>
2018-10-04 23:00:06 +0200

    Added User.class
    User.class is used to store or edit information in the realtime database. Is also used to ensure that the vital information(e-mail,...) is always correct.

--- app/src/main/java/no/hiof/informatikk/gruppe6/rusletur/User.java
+++ app/src/main/java/no/hiof/informatikk/gruppe6/rusletur/User.java
@@ -1,51 +1,87 @@
 package no.hiof.informatikk.gruppe6.rusletur;
 
+import android.util.Log;
+
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+
 /**
- * Stores the userinformation for the RTDB. Easier to store and retrieve information.
+ *                              User.class
+ * It is used as a easier way to collaborate the realtime database
+ * with information from user and Authentication database.
+ * A user.class will always be created when the user logs in.
+ *
+ * Treat User.class as the as the user.class will always store
+ * its information directly onto the UID branch.
+ *
+ *      Example:
+ *          Anna wanna updated her email. After updating the email,
+ *          the app should do:
+ *              User anna = new User(FirebaseUser);
+ *              anna.updateEmail();
+ *
+ *          Anna wanna change her phone number? Call new User()
  */
 
 public class User {
-    private String firstName;
-    private String lastName;
-    private String tlf;
-    private String email;
 
-    public User(String firstName, String lastName, String tlf, String email) {
-        this.firstName = firstName;
-        this.lastName = lastName;
-        this.tlf = tlf;
-        this.email = email;
+    private static final String TAG = "User";
+    String uid = FirebaseAuth.getInstance().getCurrentUser().getUid();
+    DatabaseReference myRef = FirebaseDatabase.getInstance().getReference();
+    DatabaseReference uidRef = myRef.child("user").child(uid);
+    String firstname;
+    String lastname;
+    String email;
+    String phone;
+    String username;
+    FirebaseUser user;
+
+    public User(FirebaseUser user) {
+        this.user = user;
     }
 
-    public String getFirstName() {
-        return firstName;
+    public void setAll(String username, String firstname, String lastname, String phone, String email) {
+
+        uidRef.child("username").setValue(username);
+        uidRef.child("firstname").setValue(firstname);
+        uidRef.child("lastname").setValue(lastname);
+        uidRef.child("phone").setValue(phone);
+        try {
+            uidRef.child("email").setValue(user.getEmail());
+            this.email = user.getEmail();
+        }
+        catch (NullPointerException e) {
+            Log.e(TAG,"getEmail() returned NULL. Is anyone logged in?");
+        }
     }
 
-    public void setFirstName(String firstName) {
-        this.firstName = firstName;
+    public void setUsername(String username) {
+        uidRef.child("username").setValue(username);
+        this.username = username;
+    }
+    public void setFirstname(String firstname) {
+        uidRef.child("firstname").setValue(firstname);
+        this.firstname = firstname;
+    }
+    public void setLastname(String lastname) {
+        uidRef.child("lastname").setValue(lastname);
+        this.lastname = lastname;
+    }
+    public void setPhone(String phone) {
+        uidRef.child("phone").setValue(phone);
+        this.phone = phone;
+    }
+    public void updateEmail() {
+        try {
+            uidRef.child("email").setValue(user.getEmail());
+            this.email = user.getEmail();
+        }
+        catch (NullPointerException e) {
+            Log.e(TAG,"getEmail() returned NULL. Is anyone logged in?");
+        }
     }
 
-    public String getLastName() {
-        return lastName;
-    }
 
-    public void setLastName(String lastName) {
-        this.lastName = lastName;
-    }
-
-    public String getTlf() {
-        return tlf;
-    }
-
-    public void setTlf(String tlf) {
-        this.tlf = tlf;
-    }
-
-    public String getEmail() {
-        return email;
-    }
-
-    public void setEmail(String email) {
-        this.email = email;
-    }
 }
