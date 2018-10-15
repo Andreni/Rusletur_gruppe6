1067ac99b451271e05a9a9fab83e001fe2ce1e09
Andreas <amikal@hotmail.no>
2018-10-15 20:08:09 +0200

    Runnable but hard coded?

--- app/src/main/java/no/hiof/informatikk/gruppe6/rusletur/fragment/RecyclerViewFragment.java
+++ app/src/main/java/no/hiof/informatikk/gruppe6/rusletur/fragment/RecyclerViewFragment.java
@@ -4,6 +4,7 @@ import android.os.Bundle;
 import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
+import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
 import android.view.LayoutInflater;
 import android.view.View;
@@ -15,6 +16,7 @@ import com.google.firebase.auth.FirebaseUser;
 import java.util.ArrayList;
 
 import no.hiof.informatikk.gruppe6.rusletur.R;
+import no.hiof.informatikk.gruppe6.rusletur.RecycleViewAdapter;
 
 public class RecyclerViewFragment extends Fragment {
 
@@ -36,13 +38,8 @@ public class RecyclerViewFragment extends Fragment {
 
 
 
+        initArraysForRecyclerView();
         return view;
-    }
-
-    private void initRecyclerView() {
-        RecyclerView recyclerView = getView().findViewById(R.id.recyclerView);
-
-
     }
 
     private void initArraysForRecyclerView(){
@@ -60,8 +57,17 @@ public class RecyclerViewFragment extends Fragment {
         mItem.add(" ");
         mItem.add("j");
 
+        initRecyclerView();
+
     }
 
+    private void initRecyclerView() {
+        RecyclerView recyclerView = getView().findViewById(R.id.recyclerView);
+        RecycleViewAdapter adapter = new RecycleViewAdapter(mItem, getContext());
+
+        recyclerView.setAdapter(adapter);
+        recyclerView.setLayoutManager(new LinearLayoutManager(getContext()));
+    }
 
 
 
