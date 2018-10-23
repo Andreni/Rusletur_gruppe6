3846efad2cefacd76ef2c53097370f6878f73a07
Andreas <amikal@hotmail.no>
2018-10-23 19:42:48 +0200

    Added functionality in method for call

--- app/src/main/java/no/hiof/informatikk/gruppe6/rusletur/ApiCalls/ApiNasjonalturbase.java
+++ app/src/main/java/no/hiof/informatikk/gruppe6/rusletur/ApiCalls/ApiNasjonalturbase.java
@@ -9,6 +9,8 @@ import com.android.volley.VolleyError;
 import com.android.volley.toolbox.JsonObjectRequest;
 import com.android.volley.toolbox.Volley;
 
+import org.json.JSONArray;
+import org.json.JSONException;
 import org.json.JSONObject;
 
 public class ApiNasjonalturbase extends AppCompatActivity{
@@ -21,12 +23,30 @@ public class ApiNasjonalturbase extends AppCompatActivity{
         JsonObjectRequest request = new JsonObjectRequest(Request.Method.GET, url, null, new Response.Listener<JSONObject>() {
             @Override
             public void onResponse(JSONObject response) {
-                
+                try {
+                    JSONArray jsonArray = response.getJSONArray("documents");
+
+                    for(int i = 0; i < jsonArray.length(); i++){
+                        JSONObject turer = jsonArray.getJSONObject(i);
+
+                        String id = turer.getString("_id");
+                        String status = turer.getString("status");
+                        String endret = turer.getString("endret");
+                        String tilbyder = turer.getString("tilbyder");
+                        JSONObject tags  = turer.getJSONObject("tags");
+                        String lisens = turer.getString("lisens");
+                        String navn = turer.getString("navn");
+
+                    }
+
+                } catch (JSONException e) {
+                    e.printStackTrace();
+                }
             }
         }, new Response.ErrorListener() {
             @Override
             public void onErrorResponse(VolleyError error) {
-
+                error.printStackTrace();
             }
         });
 
