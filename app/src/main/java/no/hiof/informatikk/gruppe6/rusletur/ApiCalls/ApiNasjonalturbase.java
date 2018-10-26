3ce97dc4d0caf6f9c5378139fd89874554092a55
Andreas <amikal@hotmail.no>
2018-10-26 12:14:07 +0200

    Added a comment

--- app/src/main/java/no/hiof/informatikk/gruppe6/rusletur/ApiCalls/ApiNasjonalturbase.java
+++ app/src/main/java/no/hiof/informatikk/gruppe6/rusletur/ApiCalls/ApiNasjonalturbase.java
@@ -8,6 +8,7 @@ import com.android.volley.Request;
 import com.android.volley.RequestQueue;
 import com.android.volley.Response;
 import com.android.volley.VolleyError;
+import com.android.volley.toolbox.JsonArrayRequest;
 import com.android.volley.toolbox.JsonObjectRequest;
 import com.android.volley.toolbox.Volley;
 
@@ -20,9 +21,10 @@ public class ApiNasjonalturbase{
 
     public static void jsonFetchTripList(Context k) {
 
+        final Context kont = k;
         RequestQueue mQueue = Volley.newRequestQueue(k);
 
-        String url = "http://dev.nasjonalturbase.no/turer?limit=5000";
+        String url = "http://dev.nasjonalturbase.no/turer?limit=1";
 
         JsonObjectRequest request = new JsonObjectRequest(Request.Method.GET, url, null, new Response.Listener<JSONObject>() {
             @Override
@@ -34,13 +36,45 @@ public class ApiNasjonalturbase{
                         JSONObject turer = jsonArray.getJSONObject(i);
 
                         String id = turer.getString("_id");
-                        String status = turer.getString("status");
+                        /*String status = turer.getString("status");
                         String endret = turer.getString("endret");
                         String tilbyder = turer.getString("tilbyder");
                         String lisens = turer.getString("lisens");
-                        String navn = turer.getString("navn");
+                        String navn = turer.getString("navn");*/
 
-                        Log.d(TAG, "onResponse: KALLL2 id: " + id + ". Navn: " + navn);
+                        Log.d(TAG, "onResponse: KALLL2 id: " + id);
+
+                        //ApiNasjonalturbase.jsonFetchIdInfo(kont, "http://dev.nasjonalturbase.no/turer/" + id);
+
+                        new JsonArrayRequest(Request.Method.GET, "dev.nasjonalturbase.no/turer/"+id, null, new Response.Listener<JSONArray>() {
+                            @Override
+                            public void onResponse(JSONArray response) {
+
+                                for(int j = 0; j < response.length(); j++){
+                                    try {
+                                        JSONObject tur = response.getJSONObject(j);
+
+                                        String id = tur.getString("_id");
+                                        String beskrivelse = tur.getString("beskrivelse");
+
+                                        Log.d(TAG, "onResponse: DOOOONE: " + id);
+                                        Log.d(TAG, "onResponse: DOOOONE: " + beskrivelse);
+
+                                    } catch (JSONException e) {
+                                        e.printStackTrace();
+                                    }
+
+                                }
+
+                            }
+                        }, new Response.ErrorListener() {
+                            @Override
+                            public void onErrorResponse(VolleyError error) {
+                                Log.d(TAG, "onErrorResponse: Error when retrieving id info");
+                                error.printStackTrace();
+                            }
+                        });
+
 
                     }
 
@@ -57,6 +91,54 @@ public class ApiNasjonalturbase{
 
         mQueue.add(request);
 
+    }
+//Kommentar!!
+    public static void jsonFetchIdInfo(Context k, String id){
+        RequestQueue idQueue = Volley.newRequestQueue(k);
+
+        String url = "dev.najsonalturbase.no/turer/" + id;
+        JsonArrayRequest request = new JsonArrayRequest(Request.Method.GET, url, null, new Response.Listener<JSONArray>() {
+            @Override
+            public void onResponse(JSONArray response) {
+
+                for(int j = 0; j < response.length(); j++){
+                    try {
+                        JSONObject tur = response.getJSONObject(j);
+
+                        String id = tur.getString("_id");
+                        String beskrivelse = tur.getString("beskrivelse");
+
+                        Log.d(TAG, "onResponse: DOOOONE: " + id);
+                        Log.d(TAG, "onResponse: DOOOONE: " + beskrivelse);
+
+                    } catch (JSONException e) {
+                        e.printStackTrace();
+                    }
+
+                }
+
+            }
+        }, new Response.ErrorListener() {
+            @Override
+            public void onErrorResponse(VolleyError error) {
+                Log.d(TAG, "onErrorResponse: Error when retrieving id info");
+                error.printStackTrace();
+            }
+        });
+        /*JsonObjectRequest request = new JsonObjectRequest(Request.Method.GET, url, null, new Response.Listener<JSONObject>() {
+            @Override
+            public void onResponse(JSONObject response) {
+
+            }
+        }, new Response.ErrorListener() {
+            @Override
+            public void onErrorResponse(VolleyError error) {
+                Log.d(TAG, "onErrorResponse: Error when retrieving id info");
+                error.printStackTrace();
+            }
+        });*/
+
+        idQueue.add(request);
     }
 
 }
