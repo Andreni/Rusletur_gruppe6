119bba50b9d530986e133ebbbb1b88223fb345af
Andreas <amikal@hotmail.no>
2018-10-30 19:22:31 +0100

    Added onCreateViewHolder method and xml file

--- app/src/main/java/no/hiof/informatikk/gruppe6/rusletur/RecyclerView/MainTripRecyclerViewAdapter.java
+++ app/src/main/java/no/hiof/informatikk/gruppe6/rusletur/RecyclerView/MainTripRecyclerViewAdapter.java
@@ -1,10 +1,16 @@
 package no.hiof.informatikk.gruppe6.rusletur.RecyclerView;
 
 import android.content.Context;
+import android.support.annotation.NonNull;
+import android.support.v7.widget.RecyclerView;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
 
 import java.util.ArrayList;
 
 import no.hiof.informatikk.gruppe6.rusletur.MapsAndTrips.Trip;
+import no.hiof.informatikk.gruppe6.rusletur.R;
 
 public class MainTripRecyclerViewAdapter extends RecyclerView<MainTripRecyclerViewAdapter.ViewHolder>{
 
@@ -16,5 +22,16 @@ public class MainTripRecyclerViewAdapter extends RecyclerView<MainTripRecyclerVi
         this.mContext = kont;
     }
 
+
+    @NonNull
+    @Override
+    public RecyclerView.ViewHolder onCreateViewHolder(@NonNull ViewGroup viewGroup, int i){
+        View view = LayoutInflater.from(viewGroup.getContext()).inflate(R.layout.layout_listitem_tripsview, viewGroup, false);
+        ViewHolder holder = new ViewHolder(view);
+
+        return holder;
+    }
+
+    
 
 }
