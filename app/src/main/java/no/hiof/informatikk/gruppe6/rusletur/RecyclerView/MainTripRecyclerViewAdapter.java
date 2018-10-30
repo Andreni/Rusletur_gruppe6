ae2b4030a9c6e218bd64858720c6e563ac5ff808
Andreas <amikal@hotmail.no>
2018-10-30 19:32:06 +0100

    Started on oonBindViewHolder

--- app/src/main/java/no/hiof/informatikk/gruppe6/rusletur/RecyclerView/MainTripRecyclerViewAdapter.java
+++ app/src/main/java/no/hiof/informatikk/gruppe6/rusletur/RecyclerView/MainTripRecyclerViewAdapter.java
@@ -6,13 +6,15 @@ import android.support.v7.widget.RecyclerView;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.RelativeLayout;
 
 import java.util.ArrayList;
+import java.util.List;
 
 import no.hiof.informatikk.gruppe6.rusletur.MapsAndTrips.Trip;
 import no.hiof.informatikk.gruppe6.rusletur.R;
 
-public class MainTripRecyclerViewAdapter extends RecyclerView<MainTripRecyclerViewAdapter.ViewHolder>{
+public class MainTripRecyclerViewAdapter extends RecyclerView.Adapter<MainTripRecyclerViewAdapter.ViewHolder>{
 
     private ArrayList<Trip> mItem;
     Context mContext;
@@ -25,13 +27,41 @@ public class MainTripRecyclerViewAdapter extends RecyclerView<MainTripRecyclerVi
 
     @NonNull
     @Override
-    public RecyclerView.ViewHolder onCreateViewHolder(@NonNull ViewGroup viewGroup, int i){
+    public ViewHolder onCreateViewHolder(@NonNull ViewGroup viewGroup, int i){
         View view = LayoutInflater.from(viewGroup.getContext()).inflate(R.layout.layout_listitem_tripsview, viewGroup, false);
         ViewHolder holder = new ViewHolder(view);
+
 
         return holder;
     }
 
-    
+    @Override
+    public void onBindViewHolder(@NonNull MainTripRecyclerViewAdapter.ViewHolder holder, int position, @NonNull List<Object> payloads) {
+        super.onBindViewHolder(holder, position, payloads);
+
+        for(Trip trip : mItem){
+            
+        }
+
+    }
+
+    @Override
+    public int getItemCount() {
+        return 0;
+    }
+
+
+    public class ViewHolder extends RecyclerView.ViewHolder{
+        RelativeLayout parentLayout;
+
+        public ViewHolder(View itemView){
+            super(itemView);
+
+            parentLayout = itemView.findViewById(R.id.parentLayoutForListItem);
+        }
+
+    }
+
+
 
 }
