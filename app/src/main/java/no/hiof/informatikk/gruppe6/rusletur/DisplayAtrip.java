b93f9568ecad1ad54de2a57e9c09749c77c61835
Andreas Nielsen <andreni@hiof.no>
2018-10-30 19:37:30 +0100

    Added displayAtripView class for displaying trip objects

---
+++ app/src/main/java/no/hiof/informatikk/gruppe6/rusletur/DisplayAtrip.java
@@ -0,0 +1,16 @@
+package no.hiof.informatikk.gruppe6.rusletur;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+/**
+ * Class used for displaying a trip object
+ */
+public class DisplayAtrip extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_display_atrip);
+    }
+}
