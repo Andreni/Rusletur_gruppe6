1c8f826235abb20dab699890a3e8f5b5d1340eb3
Andreas <amikal@hotmail.no>
2018-10-30 19:15:09 +0100

    Constructor

--- app/src/main/java/no/hiof/informatikk/gruppe6/rusletur/RecyclerView/MainTripRecyclerViewAdapter.java
+++ app/src/main/java/no/hiof/informatikk/gruppe6/rusletur/RecyclerView/MainTripRecyclerViewAdapter.java
@@ -1,4 +1,20 @@
 package no.hiof.informatikk.gruppe6.rusletur.RecyclerView;
 
+import android.content.Context;
+
+import java.util.ArrayList;
+
+import no.hiof.informatikk.gruppe6.rusletur.MapsAndTrips.Trip;
+
 public class MainTripRecyclerViewAdapter extends RecyclerView<MainTripRecyclerViewAdapter.ViewHolder>{
+
+    private ArrayList<Trip> mItem;
+    Context mContext;
+
+    public MainTripRecyclerViewAdapter(Context kont, ArrayList<Trip> turer){
+        this.mItem = turer;
+        this.mContext = kont;
+    }
+
+
 }
