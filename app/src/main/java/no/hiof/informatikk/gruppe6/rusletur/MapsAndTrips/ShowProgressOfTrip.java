dc79f6f7a01b9c0f00e2685de25d359d60baf481
bj0rnar <bj0rnar@hotmail.com>
2018-11-05 19:55:41 +0100

    Getting closer

--- app/src/main/java/no/hiof/informatikk/gruppe6/rusletur/MapsAndTrips/ShowProgressOfTrip.java
+++ app/src/main/java/no/hiof/informatikk/gruppe6/rusletur/MapsAndTrips/ShowProgressOfTrip.java
@@ -1,16 +1,21 @@
 package no.hiof.informatikk.gruppe6.rusletur.MapsAndTrips;
 
+import android.graphics.Color;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.support.v4.app.FragmentActivity;
 import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
 
+import com.google.android.gms.maps.CameraUpdate;
 import com.google.android.gms.maps.CameraUpdateFactory;
 import com.google.android.gms.maps.GoogleMap;
 import com.google.android.gms.maps.OnMapReadyCallback;
 import com.google.android.gms.maps.SupportMapFragment;
+import com.google.android.gms.maps.model.CameraPosition;
 import com.google.android.gms.maps.model.LatLng;
 import com.google.android.gms.maps.model.MarkerOptions;
+import com.google.android.gms.maps.model.PolylineOptions;
 
 import java.util.ArrayList;
 
@@ -22,6 +27,8 @@ public class ShowProgressOfTrip extends FragmentActivity implements OnMapReadyCa
 
     private GoogleMap gMap;
     private ArrayList<LatLng> receievedList;
+    private LatLng startLocation;
+    public  final String TAG = "ShowProgressOfTrip";
 
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
@@ -29,7 +36,9 @@ public class ShowProgressOfTrip extends FragmentActivity implements OnMapReadyCa
 
         setContentView(R.layout.activity_progressmap);
 
-        //receievedList = TripTracker.fetchArray();
+        receievedList = TripTracker.fetchArray();
+        Log.i(TAG, "onCreate size of array : " + String.valueOf(receievedList.size()));
+
 
         SupportMapFragment supportMapFragment = (SupportMapFragment) getSupportFragmentManager().findFragmentById(R.id.progress_map_gmap);
         supportMapFragment.getMapAsync(this);
@@ -40,10 +49,21 @@ public class ShowProgressOfTrip extends FragmentActivity implements OnMapReadyCa
     public void onMapReady(GoogleMap googleMap) {
         gMap = googleMap;
 
-        LatLng sydney = new LatLng(-34, 151);
-        gMap.addMarker(new MarkerOptions().position(sydney).title("Marker in Sydney"));
-        gMap.moveCamera(CameraUpdateFactory.newLatLng(sydney));
+        PolylineOptions poly = new PolylineOptions();
+        startLocation = new LatLng(receievedList.get(0).longitude, receievedList.get(0).longitude);
 
+        //dRAW POLYLINES.
+        for(int i=0; i < receievedList.size();i++){
+            poly.add(new LatLng(receievedList.get(i).longitude, receievedList.get(i).latitude));
+        }
+
+        //Fløtt kamera
+        gMap.moveCamera(CameraUpdateFactory.newCameraPosition(new CameraPosition(startLocation, 10, 0, 0)));
+
+        //Fargefaen
+        poly.color(Color.GREEN);
+        poly.width(10);
+        gMap.addPolyline(poly);
 
     }
 
