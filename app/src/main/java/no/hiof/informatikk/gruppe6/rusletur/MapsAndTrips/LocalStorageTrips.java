8f6c895c8676f5e9289eaa365f585f8867d50949
Andreas Nielsen <andreni@hiof.no>
2018-11-09 10:14:00 +0100

    Cleaned codd

--- app/src/main/java/no/hiof/informatikk/gruppe6/rusletur/MapsAndTrips/LocalStorageTrips.java
+++ app/src/main/java/no/hiof/informatikk/gruppe6/rusletur/MapsAndTrips/LocalStorageTrips.java
@@ -17,6 +17,11 @@ import java.util.Arrays;
 
 import no.hiof.informatikk.gruppe6.rusletur.R;
 
+/**
+ * Class for displaying stored Trip objects
+ * SQLite
+ * When the user either wants to store or load objects, the static methods are called upon
+ */
 public class LocalStorageTrips extends AppCompatActivity {
     ArrayList<LatLng> myLatLng = new ArrayList<>();
     Button btnBack;
@@ -26,102 +31,25 @@ public class LocalStorageTrips extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_local_storage_trips);
         btnBack = findViewById(R.id.localStorage_goBack_button);
-        /*double a1, a2, b1, b2, c1, c2;
-        a1 = 6.28295153901365;
-        a2 = 58.8226193213945;
-        myLatLng.add(new LatLng(a1, a2));
-        b1 = 6.29651278779295;
-        b2 = 58.8205753142577;
-        c1 = 6.30921572968748;
-        c2 = 58.8211085451286;
-        myLatLng.add(new LatLng(b1, b2));
-        myLatLng.add(new LatLng(c1, c2));
-        String[] mValue = new String[myLatLng.size()];
-        for (int i = 0; i < myLatLng.size(); i++) {
-            mValue[i] = myLatLng.get(i).latitude + " - " + myLatLng.get(i).longitude;
-        }
-
-        for (int i = 0; i < myLatLng.size(); i++) {
-            Log.d("SQLQ", myLatLng.get(i) + "OY!!");
-        }*/
-        /////////////////////////////////////////
-
-
-
 
         /*SQLiteDatabase sqLiteDatabase = getApplicationContext().openOrCreateDatabase("TripsLocal.db", MODE_PRIVATE, null);
         String sqlToInsert = "DROP TABLE trips;";
         sqLiteDatabase.execSQL(sqlToInsert);
         sqLiteDatabase.close();*/
         //Intitial run config
-        /*
 
-
-
-
-        sqLiteDatabase.execSQL("CREATE TABLE IF NOT EXISTS trips(id TEXT,navn TEXT," +
-                "tag TEXT,gradering TEXT,tilbyder TEXT,fylke TEXT,kommune TEXT,beskrivelse TEXT," +
-                "lisens TEXT, url TEXT, tidsbruk TEXT, latLng TEXT);");
-
-
-
-        Cursor cursor = sqLiteDatabase.rawQuery("SELECT * FROM trips;", null);
-        if (cursor.moveToFirst()) {
-            do {
-                String id = cursor.getString(0);
-                String name = cursor.getString(1);
-                String tag = cursor.getString(2);
-                String gradering = cursor.getString(3);
-                String tilbyder = cursor.getString(4);
-                String fylke = cursor.getString(5);
-                String kommune = cursor.getString(6);
-                String beskrivelse = cursor.getString(7);
-                String lisens = cursor.getString(8);
-                String url = cursor.getString(9);
-                String tidsbruk = cursor.getString(10);
-                String latLng = cursor.getString(11);
-
-                Log.i("SQLQ",id + "-" + name + " - " + tag + " - "
-                     );
-
-
-
-                //Fetch the string that contains the compressed Array
-                //String latLng = cursor.getString(10);
-
-
-                /*Log.d("SQLQ", latLng + " looks like this");
-
-                //Remove the [ ] from the recovered string
-                String arrRemoved = latLng.substring(1, latLng.length() - 1);
-
-                //Make a new Array by splitting the latLng points by ,
-                String[] latLngArray = arrRemoved.split(", ");
-
-                //Split simple array into Lat[0} and Long[1]
-                for (int i = 0; i < latLngArray.length; i++) {
-                    String[] latLngSplits = latLngArray[0].split(" - ");
-                    Double lat = Double.parseDouble(latLngSplits[0]);
-                    Double longt = Double.parseDouble(latLngSplits[1]);
-                    myLatLng.add(new LatLng(lat, longt));
-
-                }
-
-                for (int i = 0; i < myLatLng.size(); i++) {
-                    Log.d("SQLQ", myLatLng.get(i) + "OY!!");
-                }*/
-          /*  } while ((cursor.moveToNext()));
-
-
-        }
-        cursor.close();
-        sqLiteDatabase.close(); */
 
 
           /* Ta imot dette arrayet */
         retriveItemsFromStorage();
     }
-    //TODO Rework LatLNG Compressor
+
+    /**
+     *  When the user wants to store the obejct locally this method is used.
+     * @param  context The context from where the method is called from
+     * @param aTrip The Trip object the user wants to store is passed
+     * @return Gives back a message to the user that trip is stored
+     */
     public static String addAitemToStorage(Context context,Trip aTrip){
         SQLiteDatabase sqLiteDatabase = context.openOrCreateDatabase("TripsLocal.db", MODE_PRIVATE, null);
         Log.d("SQLQ","Attempting to write to database");
@@ -155,9 +83,6 @@ public class LocalStorageTrips extends AppCompatActivity {
     public static ArrayList<Trip> retriveItemsFromStorage(Context context,String aFylke, String aKommune){
         ArrayList<Trip> availableTrips = new ArrayList<>();
         SQLiteDatabase sqLiteDatabase = context.openOrCreateDatabase("TripsLocal.db", MODE_PRIVATE, null);
-        //Intitial run config
-        //String sqlToInsert = "DROP TABLE trips;";
-        //sqLiteDatabase.execSQL(sqlToInsert);
         sqLiteDatabase.execSQL("CREATE TABLE IF NOT EXISTS trips(id TEXT,navn TEXT," +
                 "tag TEXT,gradering TEXT,tilbyder TEXT,fylke TEXT,kommune TEXT,beskrivelse TEXT," +
                 "lisens TEXT, url TEXT, tidsbruk TEXT, latLng TEXT);");
@@ -229,23 +154,20 @@ public class LocalStorageTrips extends AppCompatActivity {
                 String url = cursor.getString(9);
                 String tidsbruk = cursor.getString(10);
                 String latLng = cursor.getString(11);
-                //Fetch the string that contains the compressed Array
-                //Remove the [ ] from the recovered string
-                Log.i("SQLQ",latLng);
+                // Fetch the string that contains the compressed Array
+                // Remove the [ ] from the recovered string
                 String arrRemoved = latLng.substring(1, latLng.length() - 1);
-                Log.i("SQLQ",arrRemoved);
-                //Make a new Array by splitting the latLng points by ,
+                // Make a new Array by splitting the latLng points by ,
                 String[] latLngArray = arrRemoved.split(", ");
-                Log.i("SQLQ",latLngArray[0]);
                 ArrayList<LatLng> arrayListLatLng = new ArrayList<>();
-                //Split simple array into Lat[0} and Long[1]
+                // Split simple array into Lat[0} and Long[1]
                 for (int i = 0; i < latLngArray.length; i++) {
                     String[] latLngSplits = latLngArray[i].split(" - ");
                     Double lat = Double.parseDouble(latLngSplits[0]);
                     Double longt = Double.parseDouble(latLngSplits[1]);
                     arrayListLatLng.add(new LatLng(lat, longt));
                 }
-                //Build the retrived trip object from storage
+                // Build the retrieved trip object from storage
                 availableTrips.add(new Trip(id,navn,tag,gradering,tilbyder,
                         fylke,kommune,beskrivelse,lisens,url,arrayListLatLng,tidsbruk));
 
