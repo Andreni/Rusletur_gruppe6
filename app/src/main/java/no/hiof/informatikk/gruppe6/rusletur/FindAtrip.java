cb3202d0837288e1b1196d126b98adaed5635408
Magnus <magnus.d.pettersen@hotmail.com>
2018-11-16 13:06:16 +0100

    Removed faulty code

--- app/src/main/java/no/hiof/informatikk/gruppe6/rusletur/FindAtrip.java
+++ app/src/main/java/no/hiof/informatikk/gruppe6/rusletur/FindAtrip.java
@@ -8,6 +8,7 @@ import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
 import android.util.Log;
 import android.view.View;
+import android.view.ViewGroup;
 import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
 import android.widget.ProgressBar;
@@ -270,7 +271,6 @@ public class FindAtrip extends AppCompatActivity  {
             onlyRusleturTrips = false;
         }
     }
-
     //Handler for notifying a new item in recycler view
     private Handler handler = new Handler();
     //It's a handler/runnablew for doing it in a new thread
@@ -278,9 +278,9 @@ public class FindAtrip extends AppCompatActivity  {
         @Override
         public void run() {
             //If antall (amaount of trips in recyclerView) is less than the amount of trips in ArrayList
+
             if(antall < turer.size()){
                 //When the loading is complete, remove progression bar
-                Collections.sort(turer);
                 pgsBar.setVisibility(View.GONE);
                 //The method for loading a new item that is in the array for the recyclerview
                 adapter.notifyItemInserted(0);
@@ -299,10 +299,9 @@ public class FindAtrip extends AppCompatActivity  {
         }
     };
 
+
     //Method for cheking new items in arraylist for recycler view
     private void checkChange(){
-        Log.d(TAG, "onResponse: Check runned");
-
         //Makes a new handler, it should run in a new thread so the UI dosnæt stop
         final Handler handler2 = new Handler();
         //Makes it so it chekcs every 3 seconds. Laggy if we constantly makes new threads?
@@ -318,7 +317,7 @@ public class FindAtrip extends AppCompatActivity  {
                 }
                 //If there isn't a new item
                 if(antall == turer.size()){
-                    Log.d(TAG, "run: onResonse: " + turer);
+                    //Log.d(TAG, "run: onResonse: " + turer);
                     //Recursion. Checking again after 3 seconds
                     checkChange();
                 }
